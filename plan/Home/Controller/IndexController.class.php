00e40ac574db04953e7db6da7e18d695f5a81799
pydo <964683112@qq.com>
2015-08-05 10:24:32 +0800

    add idea and reply controller

--- plan/Home/Controller/IndexController.class.php
+++ plan/Home/Controller/IndexController.class.php
@@ -2,7 +2,31 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover,{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+	public function index(){
+		
+		$User = M("Idea"); // 实例化User对象
+		// 查找status值为1的用户数据 以创建时间排序 返回10条数据
+		$list = $User->order('create_time desc')->select();
+		//dump($list);
+		$this->assign('list',$list);
+		$this->display();
+	}
+	public function reply($id){
+		echo $id;
+		$this->display();
+		
+	}
+    public function insert() {
+        $Form = D("Idea");
+        if ($Form->create()) {
+            if (false !== $Form->add()) {
+                $this->success('数据添加成功！');
+            } else {
+                $this->error('数据写入错误');
+            }
+        } else {
+            // 字段验证错误
+            $this->error($Form->getError());
+        }
     }
 }
