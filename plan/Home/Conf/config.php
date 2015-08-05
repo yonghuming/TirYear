724b110e89175292cbbe73a42e527d4fbff29a04
pydo <964683112@qq.com>
2015-08-05 10:24:54 +0800

    add db config

--- plan/Home/Conf/config.php
+++ plan/Home/Conf/config.php
@@ -1,4 +1,11 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+		'DB_TYPE'=>'mysql',
+		'DB_HOST'=>'localhost',
+		'DB_NAME'=>'thiryear',
+		'DB_USER'=>'root',
+		'DB_PWD'=>'langxm',
+		'DB_PORT'=>'3306',
+		'DB_PREFIX'=>'thir_',
 );
